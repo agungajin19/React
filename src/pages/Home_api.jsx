b82ff7531978313a6e4d24b681936890c22c1c90
agungajin19 <nugroho@alterra.id>
2020-01-08 15:13:54 +0700

    add deploy

--- src/pages/Home_api.jsx
+++ src/pages/Home_api.jsx
@@ -2,72 +2,65 @@ import React from 'react'
 import Header from '../components/header'
 import ListArticle from '../components/news_list'
 import TopArticle from '../components/top_article'
-import {withRouter} from 'react-router-dom'
-import {connect} from 'unistore/react'
-import {store, actions} from "../store"
+import Axios from 'axios'
 
 //News API
 const url ='https://newsapi.org/v2/top-headlines?country=id&apiKey=ed7f829e2def4c558e0831a50968ac31'
 const url1 = 'https://newsapi.org/v2/everything?apiKey=ed7f829e2def4c558e0831a50968ac31'
 
 class Home extends React.Component{
-    // state = {
-    //     listNews :[],
-    //     listNewsEverything : [],
-    //     isLoadingEverything : true,
-    //     isLoading : true,
-    //     category : 'Indonesia'
-    // }
-    componentDidMount = ()=>{
-       this.props.getTopList()
-       this.props.getEveryThing()
+    state = {
+        listNews :[],
+        listNewsEverything : [],
+        isLoadingEverything : true,
+        isLoading : true,
+        category : 'Indonesia'
     }
-    // getTopList = ()=>{
-    //     const self = this
-    //     Axios
-    //         .get(`${url}`)
-    //         // .then(function(response){
-    //         //     self.setState({listNews: response.data.articles, isLoading: false})
-    //         // })
-    //         .then(function(response){
-    //             store.setState({listNews: response.data.articles, isLoading: false})
-                
-    //         })
-    //         .catch(function(error){
-    //             store.setState({isLoading: false})
-    //         })
-    //         console.log('cek get top list',this.props.listNews)
-    // }
-    // getEveryThing = () =>{
-    //     const self = this
-    //     Axios
-    //         .get(`${url1}&q=${this.props.category}`)
-    //         .then(function(response){
-    //             store.setState({listNewsEverything: response.data.articles, isLoadingEverything: false})
-    //         })
-    //         .catch(function(error){
-    //             store.setState({isLoadingEverything: false})
-    //         })
+    componentDidMount = ()=>{
+       this.getTopList()
+       this.getEveryThing()
+    }
+    getTopList = ()=>{
+        const self = this
+        Axios
+            .get(`${url}`)
+            .then(function(response){
+                self.setState({listNews: response.data.articles, isLoading: false})
+            })
+            .catch(function(error){
+                self.setState({isLoading: false})
+            })
+    }
+    getEveryThing = () =>{
+        const self = this
+        Axios
+            .get(`${url1}&q=${this.state.category}`)
+            .then(function(response){
+                self.setState({listNewsEverything: response.data.articles, isLoadingEverything: false})
+            })
+            .catch(function(error){
+                self.setState({isLoadingEverything: false})
+            })
 
-    // }
-    // handleCategory = async (e) => {
-    //     const self = this
-    //     let keyword = e
-    //     await store.setState({category : keyword, isLoadingEverything: true})
-    //     self.getEveryThing()
-    // }
+    }
+    handleCategory = async (e) => {
+        const self = this
+        console.warn('ini buat liat target',e)
+        let keyword = e
+        await self.setState({category : keyword, isLoadingEverything: true})
+        self.getEveryThing()
+    }
 
-    // handleSearch = async (e) => {
-    //     const self = this
-    //     let keyword = e.target.value
-    //     await store.setState({category : keyword, isLoadingEverything: true })
-    //     self.getEveryThing()
-    // }
+    handleSearch = async (e) => {
+        const self = this
+        console.warn('ini buat liat target',e.target.value)
+        let keyword = e.target.value
+        await self.setState({category : keyword, isLoadingEverything: true })
+        self.getEveryThing()
+    }
 
     render(){
-        
-        const{listNews, isLoading, listNewsEverything, isLoadingEverything} = this.props
-        console.log('cekisLoading',isLoading)
+        const{listNews, isLoading, listNewsEverything, isLoadingEverything} = this.state
         
         const topHeadLines = listNews.filter(item => {
             if (item.content != null && item.urlToImage != null){
@@ -94,11 +87,10 @@ class Home extends React.Component{
         
         return(
             <div>
-                <div>cek</div>
-                {/* <div>
+                <div>
                 <Header
-                    prosesSearch={e => this.props.handleSearch(e)}
-                    category = {e => this.props.handleCategory(e)}
+                    prosesSearch={e => this.handleSearch(e)}
+                    category = {e => this.handleCategory(e)}
                     {...this.props}
                     />
                 <div className=" container row pt-5" style={{paddingLeft:'12%'}}>
@@ -109,12 +101,11 @@ class Home extends React.Component{
                         {isLoadingEverything ? <div style={{textAlign : 'center'}}>Loading ...</div> : beritaAnyar}
                     </div>
                 </div>
-            </div> */}
+            </div>
                
             </div>
 
         )
     }
 }
-
-export default connect('category, isLoading, isLoadingEverything, listNews, listNewsEverything ', actions)(withRouter(Home))
+export default Home
--- src/route/MainRoute.jsx
+++ src/route/MainRoute.jsx
@@ -1,28 +1,23 @@
 import React from 'react'
 import {Route, Switch} from "react-router-dom"
 import {BrowserRouter} from 'react-router-dom'
-import {Provider} from 'unistore/react'
 
 import Home from '../pages/Home_api'
 import NotMatch from '../pages/NotMatch'
 import Signin from '../pages/Signin'
 import Profile from '../pages/Profile'
-import { store } from "../store";
-
 
 const MainRoute = () => {
     return(
-        <Provider store={store} >
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path='/' component={Home}/>
-                    <Route exact path='/signin' component={Signin}/>
-                    <Route exact path='/profile' component={Profile}/>
-                    <Route path='/news-category/:category' component={Home}/>
-                    <Route component = {NotMatch}/>
-                </Switch>
-            </BrowserRouter>
-        </Provider>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/signin' component={Signin}/>
+                <Route exact path='/profile' component={Profile}/>
+                <Route path='/news-category/:category' component={Home}/>
+                <Route component = {NotMatch}/>
+            </Switch>
+        </BrowserRouter>
 
     )
 }
